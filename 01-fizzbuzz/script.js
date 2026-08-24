b9ce5e4f61a4fe9245276815084fc495742fb536
ampol-clerc <ampolclerc@gmail.com>
2026-08-24 12:10:13 +0200

    refactor(fizzbuzz): encapsulate logic into reusable function with parameter

--- 01-fizzbuzz/script.js
+++ 01-fizzbuzz/script.js
@@ -1,12 +1,16 @@
-// Logic
-for (let num = 1; num <= 15; num++) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (num % 3 === 0) {
-    console.log("Fizz");
-  } else if (num % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(num);
+// Logic function
+function fizzBuzzGame(number) {
+  for (let i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
   }
 }
+
+fizzBuzzGame(15);
