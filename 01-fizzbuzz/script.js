5c6d1c549415cedf48a9eb1e1c548b9a56de008b
ampol-clerc <ampolclerc@gmail.com>
2026-08-25 12:41:45 +0200

    refactor(fizzbuzz): collect results into array and return data

--- 01-fizzbuzz/script.js
+++ 01-fizzbuzz/script.js
@@ -1,16 +1,19 @@
 // Logic function
 function fizzBuzzGame(number) {
+  const result = [];
+
   for (let i = 1; i <= number; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
+      result.push("FizzBuzz");
     } else if (i % 3 === 0) {
-      console.log("Fizz");
+      result.push("Fizz");
     } else if (i % 5 === 0) {
-      console.log("Buzz");
+      result.push("Buzz");
     } else {
-      console.log(i);
+      result.push(i);
     }
   }
+  return result;
 }
 
-fizzBuzzGame(15);
+console.log(fizzBuzzGame(15));
